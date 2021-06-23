137b23c4103e7a6fdc14feb73b0b87b598d55bad
Osazee Isere <oz1.concepts@gmail.com>
2021-06-23 05:16:56 +0100

    IConeable example

--- ICloneableExamples/Program.cs
+++ ICloneableExamples/Program.cs
@@ -10,6 +10,29 @@ namespace ICloneableExamples
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("A first look at Interface \n");
+
+            //all thes classes support the Icloneable interface
+            string myString = "Hello";
+            OperatingSystem unixOS = new OperatingSystem(PlatformID.Unix,
+                new Version());
+            System.Data.SqlClient.SqlConnection sqlCnn = new System.Data.SqlClient.SqlConnection();
+
+            //therefore they can all be passed into method taking
+            //Icloneable
+            CloneMe(myString);
+            CloneMe(unixOS);
+            CloneMe(sqlCnn);
+
+
+        }
+
+        private static void CloneMe(ICloneable c)
+        {
+            //Clone whatever we get and print out name
+            object theClone = c.Clone();
+            Console.WriteLine("Your Clone is a: {0}", theClone.GetType().Name);
+
         }
     }
 }
